0506d0a9ae7d63fc3f03e80da606ed00f40e124d
leovcampos <leonardoviniciuscampos@gmail.com>
2022-10-04 19:24:06 -0300

    req02 tentativa 01

--- src/functions/talkerFunctions.js
+++ src/functions/talkerFunctions.js
@@ -5,6 +5,19 @@ async function allTalkers(_req, res) {
     res.status(200).send(talkers);
 }
 
+async function talkerById(req, res) {
+    const { id } = req.params;
+    const talkers = await readFile();
+    const talkerId = talkers.find((element) => Number(id) === element.id);
+
+    if (talkerId) {
+        return res.status(200).json(talkerId);
+    }
+
+    res.status(404).json({ message: 'Pessoa palestrante não encontrada' });
+}
+
 module.exports = {
     allTalkers,
+    talkerById,
 };
--- src/routes/talkerRouter.js
+++ src/routes/talkerRouter.js
@@ -2,10 +2,13 @@ const express = require('express');
 
 const {
     allTalkers,
+    talkerById,
 } = require('../functions/talkerFunctions');
 
 const router = express.Router();
 
 router.get('/', allTalkers);
+
+router.get('/:id', talkerById);
 
 module.exports = router;
