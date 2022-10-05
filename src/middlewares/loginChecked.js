3af582056c789caf11e72408a3ddfe86bb55fc24
leovcampos <leonardoviniciuscampos@gmail.com>
2022-10-05 13:26:42 -0300

    req04

--- src/middlewares/loginChecked.js
+++ src/middlewares/loginChecked.js
@@ -2,10 +2,10 @@ const checkEmail = (req, res, next) => {
     const { email } = req.body;
     const regex = new RegExp('[a-z0-9]+@[a-z]+\\.[a-z]{2,3}');
     if (!email) {
-        return res.status(400).send({ message: 'Email obrigatório' });
+        return res.status(400).send({ message: 'O campo "email" é obrigatório' });
     }
     if (!regex.test(email)) {
-        return res.status(400).send({ message: 'Email em formato inválido' });
+        return res.status(400).send({ message: 'O "email" deve ter o formato "email@email.com"' });
     }
     next();
 };
@@ -13,10 +13,10 @@ const checkEmail = (req, res, next) => {
 const checkPassword = (req, res, next) => {
     const { password } = req.body;
     if (!password) {
-        return res.status(400).send({ message: 'Password obrigatório!' });
+        return res.status(400).send({ message: 'O campo "password" é obrigatório' });
     }
     if (password.length < 6) {
-        return res.status(400).send({ message: 'Password curto' });
+        return res.status(400).send({ message: 'O "password" deve ter pelo menos 6 caracteres' });
     }
     next();
 };
